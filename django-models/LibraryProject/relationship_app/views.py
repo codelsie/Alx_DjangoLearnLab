da98f2f8d4ca9064aa98437acafb14748b2f63d8
codelsie <lizzyogb1@gmail.com>
2025-08-12 14:39:09 +0100

    resolved template path

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,7 +6,6 @@ def list_books(request):
     books = Book.objects.all()
     return render(request, 'list_books.html', {'books': books})
 
-
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'library_detail.html'
