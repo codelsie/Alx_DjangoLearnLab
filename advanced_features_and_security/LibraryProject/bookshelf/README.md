2a3e41136fe01f815528362ad8eb06a961d8821b
codelsie <lizzyogb1@gmail.com>
2025-08-12 15:48:54 +0100

    Resolved view

---
+++ advanced_features_and_security/LibraryProject/bookshelf/README.md
@@ -0,0 +1,8 @@
+Permissions and Groups:
+Groups:
+  - Viewers: can_view
+  - Editors: can_view, can_create, can_edit
+  - Admins: can_view, can_create, can_edit, can_delete
+
+These are enforced in views using the @permission_required decorator.
+Users are assigned to groups in Django admin.
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,31 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from .models import Book
 
-# Permissions and Groups:
-# Groups:
-#   - Viewers: can_view
-#   - Editors: can_view, can_create, can_edit
-#   - Admins: can_view, can_create, can_edit, can_delete
-#
-# These are enforced in views using the @permission_required decorator.
-# Users are assigned to groups in Django admin.
-
 @permission_required('relationship_app.can_view', raise_exception=True)
-def view_books(request):
+def book_list(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/view_books.html', {'books': books})
 
-@permission_required('relationship_app.can_create', raise_exception=True)
-def create_book(request):
-    # Logic for creating a book
-    pass
-
-@permission_required('relationship_app.can_edit', raise_exception=True)
-def edit_book(request, book_id):
-    # Logic for editing a book
-    pass
-
-@permission_required('relationship_app.can_delete', raise_exception=True)
-def delete_book(request, book_id):
-    # Logic for deleting a book
-    pass
